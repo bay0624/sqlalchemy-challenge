df689bcaedc1b824ead29de869e073e5ea217ecb
Abayomi Olujobi <bay.olu87@gmail.com>
2021-06-13 18:47:08 -0400

    update climate app

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
+from sqlalchemy.sql import text
 from sqlalchemy import create_engine, func
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
@@ -30,10 +31,10 @@ def welcome():
 @app.route("/api/v1.0/precipitation")
 def precipitation_data():
     session = Session(engine)
-    results = engine.execute("SELECT date, AVG(prcp) FROM measurement \
-        WHERE date >= '2016-08-23' \
-            GROUP BY date \
-                ORDER BY date ASC").fetchall()
+    results = session.query(Measurement.date, func.avg(Measurement.prcp)).\
+        filter(Measurement.date >= "2016-08-23").\
+            group_by(Measurement.date).\
+                order_by(Measurement.date)
     session.close()
 
     prcp_data = []
@@ -49,16 +50,16 @@ def precipitation_data():
 @app.route("/api/v1.0/stations")
 def stations_data():
     session = Session(engine)
-    results = engine.execute("SELECT station, COUNT(*) AS 'num_of_reports'\
-                                 FROM measurement GROUP BY station\
-                                 ORDER BY num_of_reports DESC").fetchall()
+    results = session.query(Measurement.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).\
+            order_by(func.count(Measurement.station).desc())
     session.close()
 
     stat_data = []
     for station, num_of_reports in results:
         stat_dict = {}
-        stat_dict["station"] = station
-        stat_dict["num_of_reports"] = num_of_reports
+        stat_dict["station_id"] = station
+        stat_dict["reports"] = num_of_reports
         stat_data.append(stat_dict)
 
     return jsonify(stat_data)
@@ -67,9 +68,9 @@ def stations_data():
 @app.route("/api/v1.0/tobs")
 def tobs_data():
     session = Session(engine)
-    results = engine.execute("SELECT date, tobs FROM measurement \
-                      WHERE station = 'USC00519281' \
-                      AND date >= '2016-08-23'").fetchall()
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == "USC00519281").\
+            filter(Measurement.date >= "2016-08-23")
     session.close()
 
     temp_data = []
@@ -81,6 +82,48 @@ def tobs_data():
 
     return jsonify(temp_data)
 
+
+@app.route("/api/v1.0/<start>")
+def start_data(start):
+
+    session = Session(engine)
+    results = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        group_by(Measurement.date).\
+            filter(Measurement.date >= start).all()
+    session.close()
+    
+    st_data = []
+    for date, min, max, avg in results:
+        st_dict = {}
+        st_dict["date"] = date
+        st_dict["min temp"] = min
+        st_dict["max temp"] = max
+        st_dict["average temp"] = avg
+        st_data.append(st_dict)
+
+    return jsonify(st_data)
+
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end_data(start, end):
+
+    session = Session(engine)
+    results = session.query(Measurement.date, func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        group_by(Measurement.date).\
+            filter(Measurement.date >= start).\
+                filter(Measurement.date <= end).all()
+    session.close()
+    
+    st_data = []
+    for date, min, max, avg in results:
+        st_dict = {}
+        st_dict["date"] = date
+        st_dict["min temp"] = min
+        st_dict["max temp"] = max
+        st_dict["average temp"] = avg
+        st_data.append(st_dict)
+
+    return jsonify(st_data)
 
 if __name__ == "__main__":
     app.run(debug=True)
